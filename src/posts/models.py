a5a6e74dee91a2545b563e284b7565526d9db64a
Vlad Stan <vlad@futuristico.io>
2017-02-10 16:25:11 +0200

    modified the model to add desription to categories

--- src/posts/models.py
+++ src/posts/models.py
@@ -12,6 +12,7 @@ class Categ(models.Model):
 	name = models.CharField(max_length=100)
 	slug = models.SlugField(unique = True)
 	cover_image = models.FileField(upload_to = 'posts', default='/media/posts/iceland.jpg')
+	description = models.TextField(max_length=1000, default="Get notified every time we publish a new tip, a new hack or a deal on Adventure Travel.")
 
 	def __str__(self):
 		return self.name
